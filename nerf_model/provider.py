d68e651864124b2e3bf9e033b2cf2a8b96aac691
sevashasla <skorokhodov.vs@phystech.edu>
2023-02-24 19:19:41 +0300

    change default test_size to 10

--- nerf_model/provider.py
+++ nerf_model/provider.py
@@ -93,7 +93,7 @@ def rand_poses(size, device, radius=1, theta_range=[np.pi/3, 2*np.pi/3], phi_ran
 
 
 class NeRFDataset:
-    def __init__(self, opt, device, type='train', downscale=1, n_test=10, semantic_remap=None):
+    def __init__(self, opt, device, type='train', downscale=1, n_test=100, semantic_remap=None):
         super().__init__()
         
         self.opt = opt
